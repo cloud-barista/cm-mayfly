444dac2e5d84a12d30b75d8d802735e34c4a3370
dev4unet <dev4unet@naver.com>
2024-11-04 06:06:45 +0000

    Add log command

---
+++ cmd/docker/log.go
@@ -0,0 +1,25 @@
+package docker
+
+import (
+	"fmt"
+
+	"github.com/cm-mayfly/cm-mayfly/common"
+	"github.com/spf13/cobra"
+)
+
+// pullCmd represents the pull command
+var logCmd = &cobra.Command{
+	Use:   "log",
+	Short: "View output from Cloud-Migrator system containers",
+	Long:  `View output from Cloud-Migrator system containers`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("\n[View output from Cloud-Migrator system containers.]")
+		cmdStr := fmt.Sprintf("COMPOSE_PROJECT_NAME=%s docker compose -f %s logs --follow %s", ProjectName, DockerFilePath, ServiceName)
+		//fmt.Println(cmdStr)
+		common.SysCall(cmdStr)
+	},
+}
+
+func init() {
+	dockerCmd.AddCommand(logCmd)
+}
